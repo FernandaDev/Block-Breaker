3cb63882e13acb33d68872b41950c5b890b5b5a1
Fernanda <tophinha@gmail.com>
2019-08-13 18:54:17 -0300

    Commit 01
    Level 1 and level 2 has been created. The paddle throw the ball, and the ball bounces around.

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle;
+    [SerializeField] Vector2 startVelocity;
+
+    Vector2 paddleDistance;
+    Rigidbody2D rb2D;
+    bool hasStarted = false;
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        paddleDistance = transform.position - paddle.transform.position;
+    }
+
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBall();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 newPos = new Vector2(paddle.transform.position.x,
+                                             paddle.transform.position.y) + paddleDistance;
+
+        transform.position = newPos;
+    }
+
+    void LaunchBall()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb2D.velocity = startVelocity;
+            hasStarted = true;
+        }
+    }
+}
---
+++ Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float xInput = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+
+        Vector2 newPos = new Vector2(xInput, transform.position.y);
+
+        newPos.x = Mathf.Clamp(newPos.x, minX, maxX);
+
+        transform.position = newPos;
+    }
+}
